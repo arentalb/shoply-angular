499d5fb975b782cd4b1fc9a169b1ad757a64d606
aren talb <aren.talb00@gmail.com>
2023-10-12 17:56:22 +0300

    Change some functionality to products component

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Category} from "../enums/category";
+import {Subcategory} from "../enums/subcategory";
 
 @Component({
   selector: 'app-products',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
+   categories: Category[] = [Category.Men, Category.Women];
+  subcategories: Subcategory[] =
+    [Subcategory.Tshirt, Subcategory.Jeans,
+      Subcategory.Shorts,Subcategory.Dresses,
+      Subcategory.Jackets, Subcategory.Shoes];
 
+
+  CategoriesState = false
+  categoryStates: boolean[] = [];
+
+  toggleCategories(){
+    this.CategoriesState = !this.CategoriesState
+  }
+
+  toggleCategory(index: number): void {
+
+
+    this.categoryStates[index] = !this.categoryStates[index];
+
+  }
+
+  isCategoryOpen(index: number): boolean {
+    return this.categoryStates[index];
+  }
+  applyFiltter(){
+
+  }
 }
+
