459dd4ec1bb4d95d57cc6e8a23199409735e9e31
aren talb <aren.talb00@gmail.com>
2023-10-14 21:05:50 +0300

    Change the cart component general structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
 import {ProductsComponent} from "./products/products.component";
@@ -6,14 +6,21 @@ import {SaleComponent} from "./sale/sale.component";
 import {CartComponent} from "./cart/cart.component";
 import {ErrorComponent} from "./error/error.component";
 import {CheckoutComponent} from "./checkout/checkout.component";
+import {FavorateDetailComponent} from "./cart/favorate-detail/favorate-detail.component";
+import {CartDetailComponent} from "./cart/cart-detail/cart-detail.component";
 
-const routes :Routes  =[
-  {path :'' ,component : HomeComponent},
-  {path :'products' ,component : ProductsComponent},
-  {path :'sale' ,component : SaleComponent},
-  {path :'cart' ,component : CartComponent},
-  {path :'checkout' ,component : CheckoutComponent},
-  {path :'**' ,component : ErrorComponent},
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'sale', component: SaleComponent},
+  {
+    path: 'cart', component: CartComponent, children: [
+      {path: 'myCart', component: CartDetailComponent},
+      {path: 'myFavorite', component: FavorateDetailComponent},
+    ]
+  },
+  {path: 'checkout', component: CheckoutComponent},
+  {path: '**', component: ErrorComponent},
 
 ]
 
@@ -22,6 +29,7 @@ const routes :Routes  =[
   imports: [
     RouterModule.forRoot(routes)
   ],
-  exports:[RouterModule]
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  pageName: string = "myCart";
+
+  constructor(private activeRoute: ActivatedRoute, private router: Router) {
+  }
+
+  ngOnInit() {
+    this.router.navigate(["myCart"], {relativeTo: this.activeRoute});
+  }
+
+  toggleTabs(currentPageName: string) {
+    this.pageName = currentPageName
+  }
 
 }
