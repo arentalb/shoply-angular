ee457f3627bcecaae129439c7ff9bf2b29cf768e
aren talb <aren.talb00@gmail.com>
2023-10-16 17:26:28 +0300

    Implement delete CartItem

--- src/app/cart/cart-detail/cart-card/cart-card.component.ts
+++ src/app/cart/cart-detail/cart-card/cart-card.component.ts
@@ -1,12 +1,45 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, HostListener, Input, OnInit, Output} from '@angular/core';
 import {CartItem} from "../../../models/cart-item";
+import {CartService} from "../../../services/cart.service";
+import {Product} from "../../../models/product";
 
 @Component({
   selector: 'app-cart-card',
   templateUrl: './cart-card.component.html',
   styleUrls: ['./cart-card.component.css']
 })
-export class CartCardComponent {
+export class CartCardComponent implements OnInit {
 
-  @Input() cartProduct: CartItem
+  @Input() cardItem: CartItem
+
+  constructor(private cartService: CartService) {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  deleteProduct(cartItem: CartItem) {
+    this.cartService.deleteCartItem(cartItem)
+  }
+
+  decQuantity(cartItem: CartItem) {
+    this.cartService.decQuantity(cartItem)
+
+  }
+
+  incQuantity(cartItem: CartItem) {
+    this.cartService.incQuantity(cartItem)
+
+  }
+
+  CheckedOrNot: boolean = false
+
+
+  toggleCheck() {
+    this.CheckedOrNot = !this.CheckedOrNot
+
+  }
+
+
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -15,6 +15,29 @@ export class CartService {
     this.productsInCart.push(newProduct)
   }
 
+  deleteCartItem(cartItem: CartItem) {
+    const index = this.productsInCart.indexOf(cartItem);
 
+    if (index !== -1) {
+      this.productsInCart.splice(index, 1);
+    }
+  }
+
+
+  decQuantity(cartItem: CartItem) {
+    const foundItem = this.productsInCart.find(item => item === cartItem);
+
+    if (foundItem && foundItem.quantity > 1) {
+      foundItem.quantity--;
+    }
+  }
+
+  incQuantity(cartItem: CartItem) {
+    const foundItem = this.productsInCart.find(item => item === cartItem);
+
+    if (foundItem && foundItem.quantity < 5) {
+      foundItem.quantity++;
+    }
+  }
 }
 
