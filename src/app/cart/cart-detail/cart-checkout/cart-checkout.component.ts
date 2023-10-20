56080ba07dc62f6047b4ca63633d52cbbfe530d4
aren talb <aren.talb00@gmail.com>
2023-10-20 13:54:27 +0300

    Implement tax calculator functionality

--- src/app/cart/cart-detail/cart-checkout/cart-checkout.component.ts
+++ src/app/cart/cart-detail/cart-checkout/cart-checkout.component.ts
@@ -8,17 +8,33 @@ import {CartService} from "../../../services/cart.service";
 })
 export class CartCheckoutComponent implements OnInit {
 
-  total = 1
+  subtotal = 0
+  tax: number = 0
+  total = 0
+
 
   constructor(private cartService: CartService) {
   }
 
   ngOnInit(): void {
-    this.total = this.cartService.totalCost
-
+    this.subtotal = this.cartService.totalCost
+    this.calculateTax()
     this.cartService.totalCostEvent.subscribe((total) => {
-      this.total = total
+      this.subtotal = total
+      this.calculateTax()
     })
   }
 
+  calculateTax() {
+    if (this.subtotal > 0 && this.subtotal <= 100) {
+      this.tax = 0;
+    } else if (this.subtotal > 100 && this.subtotal < 200) {
+      this.tax = this.subtotal / 10;
+    } else if (this.subtotal > 200 && this.subtotal < 300) {
+      this.tax = this.subtotal / 20;
+    } else {
+      this.tax = this.subtotal / 30;
+    }
+    this.total = this.subtotal + this.tax
+  }
 }
