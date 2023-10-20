c6559c38910b7b3801772fa4379dbd5fa4978a8b
aren talb <aren.talb00@gmail.com>
2023-10-20 16:54:35 +0300

    Implement checkout detail calculation

--- src/app/checkout/order/order-card/order-card.component.ts
+++ src/app/checkout/order/order-card/order-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {CartItem} from "../../../models/cart-item";
 
 @Component({
   selector: 'app-order-card',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class OrderCardComponent {
 
+  @Input() cardItem: CartItem
 }
--- src/app/checkout/order/order.component.ts
+++ src/app/checkout/order/order.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CartService} from "../../services/cart.service";
+import {CartItem} from "../../models/cart-item";
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.css']
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
 
+  productsInTheCart: CartItem[] = []
+
+  constructor(private cardService: CartService) {
+
+  }
+
+  subTotal: number = 0
+  tax: number = 0
+  total: number = 0
+  discountPercentage: number = 0
+  grandTotal: number = 0
+  coupon: string = ''
+
+  ngOnInit(): void {
+    this.productsInTheCart = this.cardService.productsInCart
+    this.calculateCosts()
+    this.calculateTax()
+  }
+
+  applyCuppon(coupon: string) {
+    if (coupon === 'abc10') {
+      this.discountPercentage = 0.1;
+      this.coupon = 'abc10'
+    } else if (coupon === 'abc20') {
+      this.discountPercentage = 0.15;
+      this.coupon = 'abc20'
+    } else {
+      this.discountPercentage = 0;
+      this.coupon = '';
+   
+    }
+    this.grandTotal = this.calculateTotal();
+  }
+
+  calculateTotal() {
+    this.total = this.subTotal + this.tax;
+    console.log(this.total * (1 - this.discountPercentage))
+    return this.total * (1 - this.discountPercentage);
+  }
+
+
+  calculateCosts() {
+    this.subTotal = 0;
+    for (const item of this.productsInTheCart) {
+      const productPrice = item.product.price;
+      const quantity = item.quantity;
+      this.subTotal += productPrice * quantity;
+    }
+
+
+  }
+
+  calculateTax() {
+    if (this.subTotal > 0 && this.subTotal <= 100) {
+      this.tax = 0;
+    } else if (this.subTotal > 100 && this.subTotal < 200) {
+      this.tax = this.subTotal / 10;
+    } else if (this.subTotal > 200 && this.subTotal < 300) {
+      this.tax = this.subTotal / 20;
+    } else {
+      this.tax = this.subTotal / 30;
+    }
+    this.total = this.subTotal + this.tax
+    this.grandTotal = this.total
+
+  }
 }
